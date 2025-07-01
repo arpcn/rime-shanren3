94c6b855db1e1df85f4677357f9a9b4711314adc
arpcn <arpcn@users.noreply.github.com>
2025-07-02 04:27:33 +0800

    Add files via upload

--- lua/dis_conversion.lua
+++ lua/dis_conversion.lua
@@ -1,19 +1,18 @@
 -- dis_conversion.lua
 -- Comes from @LightAPIs
 --    https://github.com/rime/home/issues/834#issuecomment-1458247557
-
+--    https://github.com/rime/librime/issues/376#issuecomment-1885986438
+--    https://github.com/rime/librime/issues/972    如果不用本方案，直接輸入兩次句號或逗號也可以。
 
 function dis_auto_conversion_processor(key_event, env)
-  -- 禁用「數字後的句號/冒號自動轉為英文標點」
+  -- 禁用「數字後的 句號/冒號/逗號/單引號 自動轉為英文標點」
   local keycode = key_event.keycode
   local dis_conversion = env.engine.context:get_option("dis_conversion")
-  if (dis_conversion and (keycode == 46 or keycode == 58)) then
-    -- keycode: 句號=46|冒號=58
+  if (dis_conversion and (keycode == 46 or keycode == 58 or keycode == 44 or keycode == 39)) then
+    -- keycode: 句號=46|冒號=58|逗號=44|單引號=39
 
     if (not key_event:release()) then
-      if (keycode == 46) then
-        env.engine:commit_text("")
-      elseif (keycode == 58) then
+      if (keycode == 46 or keycode == 58 or keycode == 44 or keycode == 39) then
         env.engine:commit_text("")
         -- 清除之前已輸出數字的狀態，後續 processors 不再處理轉換功能。
       end
